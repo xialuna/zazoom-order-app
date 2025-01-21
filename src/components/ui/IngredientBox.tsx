64dd9ad0388c06524e6a5f9a68a3d9336f880b1d
xialuna <xianhuicheng01@gmail.com>
2025-01-21 22:30:46 +0800

    Add styling for ingredients component

--- src/components/ui/IngredientBox.tsx
+++ src/components/ui/IngredientBox.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
-
+import { Plus } from 'lucide-react'
 export const IngredientBox = () => {
   return (
-    <div>
-      <img src='regular-dough.webp' className='w-5 h-5'></img>
-      <h3 className='text-lg'>Regular Dough</h3>
+    <div className='flex w-[170px] flex-col rounded-lg border-2 border-neutral-primary p-2.5'>
+      {/* Image Container */}
+      <div className='relative'>
+        <div className='flex items-center justify-center rounded-lg bg-neutral-primary p-2'>
+          <img src='assets/regular-dough.webp' className='w-[104px]'></img>
+        </div>
+
+        <button className='bg-neutral-emphasis absolute right-0 top-0 rounded p-0.5'>
+          <Plus className='text-onbrand-primary' />
+        </button>
+      </div>
+      {/* Title Card Container */}
+      <div className='flex flex-col gap-1'>
+        <h3 className='pt-2 text-lg font-semibold'>Regular Dough</h3>
+
+        {/* Price */}
+        <div className='flex justify-end'>
+          <div className='bg-neutral-emphasis flex items-center justify-end gap-1 rounded px-2 py-0.5 text-white'>
+            <Plus size={15} className='border-medium' />
+            <span className='text-sm font-medium'>₱150.00</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,45 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 const globalColors = {
-	brand: {
-		DEFAULT: "#FFCE74",
-		dark: "#7C2D18",
-	},
-	neutral: {
-		50: "#FFF3DC",
-		100: "#CFA49B",
-		200: "#9B5F4F",
-		300: "#7C2D18",
-	},
-	error: "#F24545",
-};
+  brand: {
+    DEFAULT: '#FFCE74',
+    dark: '#7C2D18'
+  },
+  neutral: {
+    50: '#FFF3DC',
+    100: '#CFA49B',
+    200: '#9B5F4F',
+    300: '#7C2D18'
+  },
+  error: '#F24545'
+}
 
 module.exports = {
   content: [
-    './src/app/**/*.{js,ts,jsx,tsx}',   // Add paths where Tailwind classes are used
-    './src/components/**/*.{js,ts,jsx,tsx}', // Add paths for components
+    './src/app/**/*.{js,ts,jsx,tsx}', // Add paths where Tailwind classes are used
+    './src/components/**/*.{js,ts,jsx,tsx}' // Add paths for components
   ],
   theme: {
     extend: {
       fontFamily: {
-        fredoka: ['Fredoka', 'sans-serif'],
+        fredoka: ['Fredoka', 'sans-serif']
       },
       colors: globalColors,
       backgroundColor: {
-        "brand-primary": globalColors.brand.DEFAULT,
-        "brand-primary-hover": globalColors.brand.dark,
-        "neutral-primary": globalColors.neutral[50],
+        'brand-primary': globalColors.brand.DEFAULT,
+        'brand-primary-hover': globalColors.brand.dark,
+        'neutral-primary': globalColors.neutral[50],
+        'neutral-emphasis': globalColors.neutral[200]
       },
       borderColor: {
-        "brand-primary": globalColors.brand.dark,
-        "neutral-primary": globalColors.neutral[100],
+        'brand-primary': globalColors.brand.dark,
+        'neutral-primary': globalColors.neutral[100]
       },
       textColor: {
-        "text-onbrand-primary": globalColors.neutral[50],
-        "neutral-primary": globalColors.neutral[300],
-        "neutral-secondary": globalColors.neutral[200],
-        "neutral-error": globalColors.error,
-      },
-    },
+        'onbrand-primary': globalColors.neutral[50],
+        'neutral-primary': globalColors.neutral[300],
+        'neutral-secondary': globalColors.neutral[200],
+        'neutral-error': globalColors.error
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
